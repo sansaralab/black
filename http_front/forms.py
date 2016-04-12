97b169db3c40a990b31acf7e47942c585e289299
Enapiuz <enapiuz@gmail.com>
2016-04-13 00:09:28 +0300

    Form fields autofocus

--- http_front/forms.py
+++ http_front/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 
 class FirstScreenForm(forms.Form):
-    identity = forms.CharField(label="На кого жалуемся?", widget=forms.TextInput(attrs={'placeholder': 'На кого жалуемся?'}))
+    identity = forms.CharField(label="На кого жалуемся?", widget=forms.TextInput(attrs={'placeholder': 'На кого жалуемся?', 'autofocus': ''}))
 
 
 class ConcreteForm(forms.Form):
     identity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'На кого жалуемся?'}))
-    title = forms.CharField(widget=forms.TextInput(attrs={'class': ''}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'autofocus': ''}))
     category = forms.CharField(widget=forms.TextInput(attrs={'class': ''}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': ''}))
